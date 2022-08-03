efb66b5c4f7b3f66a698805f537671911a259bf6
Aidan Israel <aidandisrael@gmail.com>
2022-08-02 22:47:38 -0700

    dom pt2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const element = document.querySelector("main#main");
+element.parentElement.removeChild(element);
+
+let newHeader = document.createElement("h1");
+newHeader.innerHTML = `<h1>Aidan is the champion</h1>`;
+newHeader.setAttribute("id", "victory");
+
+document.body.appendChild(newHeader);
